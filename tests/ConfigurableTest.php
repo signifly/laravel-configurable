5b84c419a0d10e6099e83a611c3176095c2c0a1e
Morten Poul Jensen <mpj@signifly.com>
2018-05-14 09:55:27 +0200

    Add test for count and all methods

--- tests/ConfigurableTest.php
+++ tests/ConfigurableTest.php
@@ -76,6 +76,37 @@ class ConfigurableTest extends TestCase
         $this->assertFalse($user->fresh()->config()->has('some_key'));
     }
 
+    /** @test */
+    function it_can_count_attributes_in_config()
+    {
+        $user = $this->createUser(['config' =>  [
+            'some_key' => 'some val',
+            'some_array' => [
+                'nested' => 'nested value',
+            ],
+        ]]);
+
+        $this->assertEquals($user->config()->count(), 2);
+    }
+
+    /** @test */
+    function it_can_retrieve_all_attributes_from_config()
+    {
+        $user = $this->createUser(['config' =>  [
+            'some_key' => 'some val',
+            'some_array' => [
+                'nested' => 'nested value',
+            ],
+        ]]);
+
+        $this->assertEquals($user->config()->all(), [
+            'some_key' => 'some val',
+            'some_array' => [
+                'nested' => 'nested value',
+            ],
+        ]);
+    }
+
     protected function createUser(array $overwrites = [])
     {
         return User::create(array_merge([
